01b4949d7fe64706dd2771f055a4a2eb6399082c
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-01-08 19:32:51 +0100

    mas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import javax.swing.text.html.StyleSheet;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -217,11 +218,9 @@ import java.util.Scanner;
             }
 }
       }
-         */
-        class Main {
-            public static void main(String[] args) {
 
-      Scanner scan = new Scanner(System.in);
+      game
+        Scanner scan = new Scanner(System.in);
       int t = (int)(Math.random() * 100) + 1;
       int n;
       do {
@@ -236,9 +235,74 @@ import java.util.Scanner;
               System.out.println("you are a winner!");
           }}
           while (n != t);
-                scan.close();
+                scan.close();x
+
+         */
+        class Main {
+            public static void main(String[] args) {
+
+
+              Scanner scan = new Scanner(System.in);
+//                int userNumber = scan.nextInt();
+                int[][] numbers = new int[3][3];
+                Random random = new Random();
+//                int sumNumber = 0;
+
+
+
+                for (int i = 0; i < numbers.length; i ++ ){
+                    for (int j = 0; j < numbers.length; j ++ ){
+                        numbers[i][j] = random.nextInt(10) + 1;
+                        System.out.print( numbers[i][j] + "\t");
+
+                    }
+                    System.out.println();
+                }
+                for (int i = 0; i < numbers.length; i ++ ){
+                    int sumLine = 0;
+                    for (int j = 0; j < numbers.length; j ++ ){
+                    sumLine += numbers[i][j];
+                        }
+                    System.out.println("sum line number "+ (i + 1) + " is "+ sumLine);
+                }
+
+                for (int j = 0; j < numbers.length; j ++ ){
+                    int sumCollum = 0;
+                    for (int i = 0; i < numbers.length; i ++ ){
+                        sumCollum += numbers[i][j];
+                    }
+                    System.out.println("sum column number "+ (j + 1) + " is "+ sumCollum);
+                }
+
+
+
+//                Arrays.sort(numbers);
+
+//                for(byte i = 0; i < numbers.length; i++){
+//                    System.out.println("Enter element" + i + ":");
+//                    numbers[i] = scan.nextInt();
+//                }
+//                for(byte i = 0; i < numbers.length; i++)
+//                    System.out.print(numbers[i] + " ");
+
+//                for( byte i = 0; i < numbers.length; i++){
+//
+//                    if(maxNumber < numbers[i]){
+//                        maxNumber = numbers[i];
+//
+//                    }
+//                    if(minNumber > numbers[i]){
+//                        minNumber = numbers[i];
+//                    }
+//                }
+
+                }
+
+
+
+
             }
-}
+
 
 
 
