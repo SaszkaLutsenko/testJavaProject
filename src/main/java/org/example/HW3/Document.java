983dd42ba77235e8187ce0be3d4b34c1bbd51a1e
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-01-26 13:36:22 +0100

    repetition java

--- src/main/java/org/example/HW3/Document.java
+++ src/main/java/org/example/HW3/Document.java
@@ -1,19 +1,1 @@
-package org.example.HW3;
 
-public class Document implements Serializable{
-
-    public  String title;
-    public  String content;
-
-    public Document(String title, String content){
-        this.content = content;
-        this.title = title;
-    }
-
-    void save(Document doc){
-        if(Serializable)
-            System.out.println("save");
-        else
-            System.out.println("this document didnt Serialible" );
-    }
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,25 @@ package org.example;
 class Main {
             public static void main(String[] args) {
 
+                // i want larn development because i want to start work in it
+                // now i watch lesson from youtube , somefeng polish bloger
+                // and i watch 11 lessons like 40 minutes and i must write script and save
+                // this task at GitHub.
+
+              String proffecion = "developer";
+              String work = "IT";
+              String platforme = "YouTube";
+              String from = "polish";
+              byte integerLessons = 11;
+              String platformeForSave = "GitHub";
+              byte time = 40;
+
+
+                System.out.println("i want larn " + proffecion + "because i want to start work in " + work +
+                        " now i watch lesson from " + platforme + " , something "+ from + " blogger" +
+                        " and i watch "+ integerLessons +" lessons like " + time + " minutes and i must write script and save" +
+                        " this task at " + platformeForSave + ".");
+
             }};
 
 
