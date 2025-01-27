a7876539300cdc63d46e00b4e9e1948bb70824b9
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-01-27 17:14:38 +0100

    larn

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 
+import java.util.Scanner;
+
 class Main {
             public static void main(String[] args) {
 
@@ -9,13 +11,28 @@ class Main {
                 // and i watch 11 lessons like 40 minutes and i must write script and save
                 // this task at GitHub.
 
-              String proffecion = "developer";
-              String work = "IT";
-              String platforme = "YouTube";
-              String from = "polish";
-              byte integerLessons = 11;
-              String platformeForSave = "GitHub";
-              byte time = 40;
+                Scanner scr = new Scanner(System.in);
+
+                System.out.println("what is your name?");
+                String proffecion = scr.nextLine();
+
+                System.out.println("where you want work?");
+                String work = scr.nextLine();
+
+                System.out.println("where you watch lessons?");
+                String platforme = scr.nextLine();
+
+                System.out.println("from what country this blogger?");
+                String from = scr.nextLine();
+
+                System.out.println("How mach lessons you see today?");
+              byte integerLessons = scr.nextByte();
+
+                System.out.println("where yuo save homework?");
+              String platformeForSave = scr.nextLine();
+
+                System.out.println("how mach time you lorn?");
+              byte time = scr.nextByte();
 
 
                 System.out.println("i want larn " + proffecion + "because i want to start work in " + work +
