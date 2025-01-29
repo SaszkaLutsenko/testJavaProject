ab40231c8223b8ef296965d5458d3cd24631738b
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-01-29 17:33:16 +0100

    calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,52 +1,55 @@
 package org.example;
 
-
 import java.util.Scanner;
+
 
 class Main {
             public static void main(String[] args) {
-
-                // i want larn development because i want to start work in it
-                // now i watch lesson from youtube , somefeng polish bloger
-                // and i watch 11 lessons like 40 minutes and i must write script and save
-                // this task at GitHub.
-
                 Scanner scr = new Scanner(System.in);
 
-                System.out.println("what is your name?");
-                String proffecion = scr.nextLine();
+//                System.out.println("Enter first name :");
+//                String firstName = scr.nextLine();
+//
+//                System.out.println("Enter second name : ");
+//                String secondName = scr.nextLine();
+//
+//                System.out.println("How old you are?");
+//                byte age = scr.nextByte();
+//
+//                System.out.println("Hello " + firstName + " " + secondName + " you have " + age + " year.");
 
-                System.out.println("where you want work?");
-                String work = scr.nextLine();
+                System.out.println("Enter number a :");
+                int a = scr.nextInt();
 
-                System.out.println("what is your name?");
-                String proffecione = scr.nextLine();
+                System.out.println("Enter number b :");
+                int b = scr.nextInt();
+                scr.nextLine();
 
-                System.out.println("where you want work?");
-                String worke = scr.nextLine();
+                System.out.println("Enter symbol ( +, -, *, /) :");
+                String symbol = scr.nextLine();
 
-                System.out.println("where you watch lessons?");
-                String platforme = scr.nextLine();
+                if(symbol.equals("+")) {
+                    int res = a + b;
+                    System.out.println(res);
+                } else if (symbol.equals("-")) {
+                    int res = a - b;
+                    System.out.println(res);
+                } else if (symbol.equals("*")) {
+                    int res = a * b;
+                    System.out.println(res);
+                } else if (symbol.equals("/") && b != 0) {
+                    int res = a / b;
+                    System.out.println(res);
+                };
 
-                System.out.println("from what country this blogger?");
-                String from = scr.nextLine();
-
-                System.out.println("How mach lessons you see today?");
-              byte integerLessons = scr.nextByte();
-
-                System.out.println("where yuo save homework?");
-              String platformeForSave = scr.nextLine();
-
-                System.out.println("how mach time you lorn?");
-              byte time = scr.nextByte();
 
 
-                System.out.println("i want larn " + proffecion + "because i want to start work in " + work +
-                        " now i watch lesson from " + platforme + " , something "+ from + " blogger" +
-                        " and i watch "+ integerLessons +" lessons like " + time + " minutes and i must write script and save" +
-                        " this task at " + platformeForSave + "." + proffecione + worke);
 
-            }};
+
+
+
+
+            }}
 
 
 
