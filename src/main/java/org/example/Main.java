7b31c6947ffdb9d92fa59eba9e08d1ece19941a4
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-01-30 19:23:20 +0100

    deskriment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 
@@ -18,36 +21,48 @@ class Main {
 //
 //                System.out.println("Hello " + firstName + " " + secondName + " you have " + age + " year.");
 
-                System.out.println("Enter number a :");
-                int a = scr.nextInt();
+//                System.out.println("Enter number a :");
+//                int a = scr.nextInt();
+//
+//                System.out.println("Enter number b :");
+//                int b = scr.nextInt();
+//                scr.nextLine();
+//
+//                System.out.println("Enter symbol ( +, -, *, /) :");
+//                String symbol = scr.nextLine();
+//
+//                if(symbol.equals("+")) {
+//                    int res = a + b;
+//                    System.out.println(res);
+//                } else if (symbol.equals("-")) {
+//                    int res = a - b;
+//                    System.out.println(res);
+//                } else if (symbol.equals("*")) {
+//                    int res = a * b;
+//                    System.out.println(res);
+//                } else if (symbol.equals("/") && b != 0) {
+//                    int res = a / b;
+//                    System.out.println(res);
+//                };
+//
+                double a = 7.5;
+                double b = 20.0;
 
-                System.out.println("Enter number b :");
-                int b = scr.nextInt();
-                scr.nextLine();
+                double c = (b / a);
+                System.out.println(c);
+                System.out.println(Math.round(c));
+                System.out.println(Math.ceil(c));
+                System.out.println(Math.floor(c));
+                System.out.println(Math.round(c * 100.0) / 100.0);
 
-                System.out.println("Enter symbol ( +, -, *, /) :");
-                String symbol = scr.nextLine();
+                BigDecimal roundNumber = new BigDecimal(c).setScale(2, RoundingMode.HALF_UP);
+                System.out.println(roundNumber.doubleValue());
 
-                if(symbol.equals("+")) {
-                    int res = a + b;
-                    System.out.println(res);
-                } else if (symbol.equals("-")) {
-                    int res = a - b;
-                    System.out.println(res);
-                } else if (symbol.equals("*")) {
-                    int res = a * b;
-                    System.out.println(res);
-                } else if (symbol.equals("/") && b != 0) {
-                    int res = a / b;
-                    System.out.println(res);
-                };
+                DecimalFormat decimalFormat = new DecimalFormat("###.##");
+                System.out.println(decimalFormat.format(c));
 
-
-
-
-
-
-
+                String formated = String.format("%.2f", c);
+                System.out.println(formated);
 
             }}
 
