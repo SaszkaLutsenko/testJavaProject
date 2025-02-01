83646358fcec350fdbf0c9967a7a2691538f66be
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-02-01 15:44:25 +0100

    end cours java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -45,15 +45,15 @@ class Main {
 //                    System.out.println(res);
 //                };
 //
-               String text = "    jak nauczyc sie programowania   ";
-               text = text.strip().toUpperCase();
-//                text = text.strip().toUpperCase().substring(4);
-
-               String[] words = text.split(" ", 2);
-               String res = words.length > 1 ? words[1] : "";
-                System.out.println(res);
-
-               }
+//               String text = "    jak nauczyc sie programowania   ";
+//               text = text.strip().toUpperCase();
+////                text = text.strip().toUpperCase().substring(4);
+//
+//               String[] words = text.split(" ", 2);
+//               String res = words.length > 1 ? words[1] : "";
+//                System.out.println(res);
+//
+//               }
 ////               text = text.replace(" ", "+");
 //               int lenght = text.length();
 //               text = text.strip();
@@ -63,10 +63,48 @@ class Main {
 //                boolean endsWithY = text.endsWith("y");
 //                System.out.println(startWithI);
 //                System.out.println(endsWithY);
+                Scanner scaner = new Scanner(System.in);
+                System.out.println("What color(red, yellow, green?");
+                String color = scaner.nextLine();
+
+                switch (color){
+                    case "red" -> System.out.println("stop");
+                    case "yellow" -> System.out.println("wait");
+                    case "green" -> System.out.println("run");
+                    default -> System.out.println("dont know this color");
+                }
+
+                int i = 0;
+                while (i < 10){
+                    System.out.println("write");
+                    i++;
+                }
+
+//                System.out.println("What is your name?");
+//                String name = scaner.nextLine();
+//
+//                if(color.equals("red") && name.endsWith("a")){
+//                    System.out.println("stop, madame");
+//                } else if (color.equals("red") && !name.endsWith("a")) {
+//                    System.out.println("stop, mister");
+//                } else if (color.equals("yellow") && name.endsWith("a")){
+//                    System.out.println("wait, madame");
+//                } else if (color.equals("yellow") && !name.endsWith("a")) {
+//                    System.out.println("wait, mister");
+//                }  else if(color.equals("green") && name.endsWith("a")){
+//                    System.out.println("run, madame");
+//                } else if (color.equals("green") && !name.endsWith("a")) {
+//                    System.out.println("run, mister");}
+//                else{
+//                    System.out.println("i dont know this color?");
+//                }
+            }}
 
 
 
-            }
+
+
+
 
 
 
