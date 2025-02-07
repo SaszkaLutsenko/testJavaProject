db6677167417f5f7f079dbecd61e13b0cff03d76
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2025-02-07 19:25:06 +0100

    list set map

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.Scanner;
+import java.util.*;
 
 
 class Main {
@@ -63,22 +63,22 @@ class Main {
 //                boolean endsWithY = text.endsWith("y");
 //                System.out.println(startWithI);
 //                System.out.println(endsWithY);
-                Scanner scaner = new Scanner(System.in);
-                System.out.println("What color(red, yellow, green?");
-                String color = scaner.nextLine();
-
-                switch (color){
-                    case "red" -> System.out.println("stop");
-                    case "yellow" -> System.out.println("wait");
-                    case "green" -> System.out.println("run");
-                    default -> System.out.println("dont know this color");
-                }
-
-                int i = 0;
-                while (i < 10){
-                    System.out.println("write");
-                    i++;
-                }
+//                Scanner scaner = new Scanner(System.in);
+//                System.out.println("What color(red, yellow, green?");
+//                String color = scaner.nextLine();
+//
+//                switch (color){
+//                    case "red" -> System.out.println("stop");
+//                    case "yellow" -> System.out.println("wait");
+//                    case "green" -> System.out.println("run");
+//                    default -> System.out.println("dont know this color");
+//                }
+//
+//                int i = 0;
+//                while (i < 10){
+//                    System.out.println("write");
+//                    i++;
+//                }
 
 //                System.out.println("What is your name?");
 //                String name = scaner.nextLine();
@@ -98,6 +98,53 @@ class Main {
 //                else{
 //                    System.out.println("i dont know this color?");
 //                }
+
+                List<Integer> numbers = new LinkedList<>();
+                numbers.add(1);
+                numbers.add(2);
+                numbers.add(3);
+                numbers.add(4);
+                numbers.add(5);
+                System.out.println(numbers);
+
+
+                Set<Integer> numbersSet = new TreeSet<>();
+
+                numbersSet.add(11);
+                numbersSet.add(22);
+                numbersSet.add(13);
+                numbersSet.add(10);
+                numbersSet.add(1);
+
+                System.out.println(numbersSet);
+
+                Map<Integer, String> map = new HashMap<>();
+                map.put(1, "one");
+                map.put(2, "two");
+                map.put(3, "thee");
+                map.put(4, "four");
+                System.out.println(map);
+
+                Stack<Integer> stack = new Stack<>();
+
+                stack.add(2);
+                stack.add(4);
+                stack.add(11);
+                stack.add(10);
+                System.out.println(stack.pop());
+                System.out.println(stack.peek());
+
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(2);
+                queue.add(4);
+                queue.add(55);
+                queue.add(3);
+
+                System.out.println(queue.peek());
+                System.out.println(queue.poll());
+
+
+
             }}
 
 
